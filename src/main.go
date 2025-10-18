e0b5eda9a9f742c35e4577f3b11164fd662be9da
Flavio Grilli <flavio.grilli@ynov.com>
2025-10-18 15:23:58 +0200

    fix : Menu et Product terminés

--- src/main.go
+++ src/main.go
@@ -5,7 +5,23 @@ import (
 	"html/template"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 )
+
+type Product struct {
+	ID          int
+	Name        string
+	Price       float64
+	Desc        string
+	InSale      bool
+	PicturePath string
+}
+
+func (prod Product) Apply50Discount() string {
+	result := prod.Price / 2
+	return fmt.Sprintf("%.2f", result)
+}
 
 func main() {
 
@@ -15,23 +31,44 @@ func main() {
 		os.Exit(1)
 	}
 
-	type Product struct {
-		Name        string
-		Price       float64
-		InSale      bool
-		PicturePath string
-	}
 	listProducts := []Product{
-		{"Palace Pull A Capuche Unisexe Chasseur", 149.99, false, "/static/img/products/19A.webp"},
-		{"Palace Washed Terry 1/4 Placket Hood Mojito", 169.99, false, "/static/img/products/16A.webp"},
-		{"Palace Pull A Capuchon Marine", 139.99, false, "/static/img/products/21A.webp"},
-		{"Palace Pantalon Bossy Jean Stone", 124.99, false, "/static/img/products/34B.webp"},
-		{"Palace Pull Crew Passepose Noir", 129.99, true, "/static/img/products/18A.webp"},
-		{"Palace Pantalon Cargo Gore-Tex, R-Tek Noir", 109.99, false, "/static/img/products/33B.webp"},
+		{1, "Palace Pull A Capuche Unisexe Chasseur", 150, "Un hoodie confortable et polyvalent au design sobre. Parfait pour un look streetwear élégant et décontracté.", false, "/static/img/products/19A.webp"},
+		{2, "Palace Washed Terry 1/4 Placket Hood Mojito", 170, "Sweat à capuche léger en coton lavé, finition “washed” pour un effet vintage. Teinte mojito rafraîchissante pour l'été.", false, "/static/img/products/16A.webp"},
+		{3, "Palace Pull A Capuchon Marine", 140, "Classique intemporel signé Palace, coloris bleu marine profond. Confort maximal pour un style urbain discret.", false, "/static/img/products/21A.webp"},
+		{4, "Palace Pantalon Bossy Jean Stone", 125, "Jean coupe droite au ton stone délavé. Idéal pour un look décontracté sans négliger la qualité du denim Palace.", false, "/static/img/products/34B.webp"},
+		{5, "Palace Pull Crew Passepose Noir", 130, "Sweat col rond noir avec détails passepoilés contrastants. Allie minimalisme et élégance streetwear.", true, "/static/img/products/18A.webp"},
+		{6, "Palace Pantalon Cargo Gore-Tex, R-Tek Noir", 110, "Pantalon technique résistant à l’eau grâce au tissu Gore-Tex. Parfait mélange entre performance et style utilitaire.", false, "/static/img/products/33B.webp"},
 	}
 
-	http.HandleFunc("/menu", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.String() != "/" {
+			// Gestion d'erreur à faire
+		}
 		listTemplates.ExecuteTemplate(w, "menu", listProducts)
+	})
+
+	http.HandleFunc("/product/", func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		idStr := strings.TrimPrefix(path, "/product/")
+		idInt, err := strconv.Atoi(idStr)
+
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+		var selected Product
+		for _, p := range listProducts {
+			if p.ID == idInt {
+				selected = p
+				break
+			}
+		}
+		if selected.ID == 0 {
+			http.NotFound(w, r)
+			return
+		}
+
+		listTemplates.ExecuteTemplate(w, "product", selected)
 	})
 
 	path, _ := os.Getwd()
